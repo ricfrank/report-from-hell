267dc71dfcc73575035e619e1f7bdde62710bbfb
ricfrank <riccardo.franconi@gmail.com>
2017-04-29 16:50:14 +0200

    add node server for webpack prod build test purpose

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.register(require('inert'), (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file(__dirname + '/index.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'public',
+        listing: true
+      }
+    }
+  });
+
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+
+    console.log('Server running at:', server.info.uri);
+  });
+});
