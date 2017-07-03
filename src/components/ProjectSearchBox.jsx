ca94af4b74254c05dcfe5b831488a2fd585753f3
ricfrank <riccardo.franconi@gmail.com>
2017-07-03 14:18:37 +0200

    reset project full texr search

--- src/components/ProjectSearchBox.jsx
+++ src/components/ProjectSearchBox.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
-const ProjectSearchBox = ({onSearchProject}) => (
-  <input onKeyUp={(event) => {onSearchProject(event.target.value)}}
-         className="form-control rfh-search--project-box" type="text" placeholder="Search project"/>
-);
+class ProjectSearchBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchValue: ''
+    }
+  }
+
+  render() {
+    return (
+      <div style={{position: 'relative'}}>
+        <input onKeyUp={(event) => {this.props.onSearchProject(event.target.value)}}
+               className="form-control rfh-search--project-box"
+               type="text"
+               placeholder="Search project"
+               value={this.state.searchValue}
+               onChange={(event) => {this.setState({searchValue: event.target.value})}}
+        />
+        <span
+          style={{right: '5px', top: '12px', position: 'absolute'}}
+          className={'glyphicon glyphicon-remove-sign'}
+          onClick={(event) => {
+            this.setState({searchValue: ''});
+            this.props.onSearchProject('');
+          }}
+        />
+      </div>
+    )
+  }
+}
 
 export default ProjectSearchBox;
