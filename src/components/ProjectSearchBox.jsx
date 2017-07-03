6b9a995c69944713210252b8a2ae8442c96ea458
ricfrank <riccardo.franconi@gmail.com>
2017-07-03 22:11:36 +0200

    reset project full text search

--- src/components/ProjectSearchBox.jsx
+++ src/components/ProjectSearchBox.jsx
@@ -11,17 +11,19 @@ class ProjectSearchBox extends React.Component {
   render() {
     return (
       <div style={{position: 'relative'}}>
-        <input onKeyUp={(event) => {this.props.onSearchProject(event.target.value)}}
-               className="form-control rfh-search--project-box"
+        <input className="form-control rfh-search--project-box"
                type="text"
                placeholder="Search project"
                value={this.state.searchValue}
-               onChange={(event) => {this.setState({searchValue: event.target.value})}}
+               onChange={(event) => {
+                 this.setState({searchValue: event.target.value});
+                 this.props.onSearchProject(event.target.value);
+               }}
         />
         <span
           style={{right: '5px', top: '12px', position: 'absolute'}}
           className={'glyphicon glyphicon-remove-sign'}
-          onClick={(event) => {
+          onClick={() => {
             this.setState({searchValue: ''});
             this.props.onSearchProject('');
           }}
