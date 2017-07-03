a63aa9233bf160a2f26b28d250b53a0e9ff625b1
ricfrank <riccardo.franconi@gmail.com>
2017-07-03 22:08:54 +0200

    reset issues full texr search

--- src/components/IssueSearchBox.jsx
+++ src/components/IssueSearchBox.jsx
@@ -3,13 +3,32 @@ import React from 'react'
 class IssueSearchBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchValue: ''
+    }
   }
 
   render() {
     return (
       <div className="col-md-12">
-        <input onKeyUp={(event) => {this.props.onSearchIssue(event.target.value)}}
-               className="form-control rfh-search-box" type="text" placeholder="Search yuor issue"/>
+        <input
+          onChange={(event) => {
+            this.props.onSearchIssue(event.target.value);
+            this.setState({searchValue: event.target.value})
+          }}
+          className="form-control rfh-search-box"
+          type="text"
+          placeholder="Search yuor issue"
+          value={this.state.searchValue}
+        />
+        <span
+          style={{right: '25px', top: '26px', position: 'absolute'}}
+          className={'glyphicon glyphicon-remove-sign'}
+          onClick={() => {
+            this.setState({searchValue: ''});
+            this.props.onSearchIssue('');
+          }}
+        />
       </div>
     )
   }
