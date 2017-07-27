5514b2bc71976f9e5912198e80d485ac2f2ee671
ricfrank <riccardo.franconi@gmail.com>
2017-07-27 07:42:42 +0200

    add webpacak config for desktop

---
+++ webpack.desktop.production.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        include: __dirname,
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      ENDPOINT: JSON.stringify('https://project.ideato.it'),
+      IS_DESKTOP: true
+    }),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+      hash: true
+    })
+  ]
+};
