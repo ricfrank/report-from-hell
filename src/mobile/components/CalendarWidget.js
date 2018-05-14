87c1735bd77345298a037b800595694bdccbd2ec
George Karametas <georgios.karametas@ideato.info>
2018-05-14 16:40:07 +0200

    Testing styles for calendar

--- src/mobile/components/CalendarWidget.js
+++ src/mobile/components/CalendarWidget.js
@@ -6,8 +6,32 @@ export default class CalendarWidget extends Component<Props> {
   render() {
     return (
       <View>
-        <Calendar />
+        <Calendar theme={themes.overrides} />
       </View>
     )
+  }
+}
+
+const themes = {
+  overrides: {
+    backgroundColor: '#38324e',
+    calendarBackground: '#38324e',
+    textSectionTitleColor: '#b6c1cd',
+    selectedDayBackgroundColor: '#ffffff',
+    selectedDayTextColor: '#ffffff',
+    todayTextColor: '#ffffff',
+    dayTextColor: '#ffffff',
+    textDisabledColor: '#d9e1e8',
+    dotColor: '#ffffff',
+    selectedDotColor: '#ffffff',
+    arrowColor: 'orange',
+    monthTextColor: 'blue',
+    textDayFontFamily: 'monospace',
+    textMonthFontFamily: 'monospace',
+    textDayHeaderFontFamily: 'monospace',
+    textMonthFontWeight: 'bold',
+    textDayFontSize: 16,
+    textMonthFontSize: 24,
+    textDayHeaderFontSize: 16
   }
 }
