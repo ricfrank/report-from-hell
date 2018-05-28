bddb91e35e1fa4ec07990c873df0511193652dc6
George Karametas <georgios.karametas@ideato.info>
2018-05-28 17:36:32 +0200

    Show log entries on calendar

--- src/mobile/components/CalendarWidget.js
+++ src/mobile/components/CalendarWidget.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { Calendar } from 'react-native-calendars'
+import { connect } from 'react-redux'
+import _ from 'lodash'
+import moment from 'moment'
 import Arrow from './Arrow'
 
-export default class CalendarWidget extends Component {
+class CalendarWidget extends Component {
+  getUserLogTimeEntries() {
+    const logEntries = {}
+    for (let log of this.props.userLogTimeEntries) {
+      logEntries[log.spentOn] = { marked: true }
+    }
+
+    return logEntries
+  }
+
   render() {
     return (
       <View>
@@ -11,6 +23,7 @@ export default class CalendarWidget extends Component {
           theme={themes.overrides}
           firstDay={1}
           renderArrow={direction => <Arrow direction={direction} />}
+          markedDates={this.getUserLogTimeEntries()}
         />
       </View>
     )
@@ -53,3 +66,29 @@ const themes = {
     }
   }
 }
+
+const mapStateToProps = state => {
+  if (!_.isEmpty(state.user.error)) {
+    alert(state.user.error.data + '\n' + state.user.error.status + '\n')
+    return
+  }
+  if (!_.isEmpty(state.userLogTimeEntries.error)) {
+    alert(
+      state.user.userLogTimeEntries.data +
+        '\n' +
+        state.user.userLogTimeEntries.status +
+        '\n'
+    )
+    return
+  }
+
+  return {
+    user: state.user,
+    projects: state.projects,
+    userLogTimeEntries: state.userLogTimeEntries,
+    loggedIssueId: state.projectIssues.loggedIssueId,
+    loggedTimeEntryId: state.projectIssues.loggedTimeEntryId
+  }
+}
+
+export default connect(mapStateToProps, null)(CalendarWidget)
